c699247918700dd1855d0f278316c28acadefbd4
Vignesh-089 <solaivignesh60@gmail.com>
2025-09-04 23:20:49 +0530

    Changes added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 5000;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running 🚀");
-});
+// Routes
+// app.use("/api/users", require("./routes/userRoutes"));
 
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
